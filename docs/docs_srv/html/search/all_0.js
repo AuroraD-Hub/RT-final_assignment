279bd55f14a1523c4856a0c210130b80bfd67793
AuroraD-Hub <72380912+AuroraD-Hub@users.noreply.github.com>
2022-03-25 16:27:43 +0000

    Delete docs/docs_srv directory

--- docs/docs_srv/html/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['call_5fsrv_2ecpp_0',['call_srv.cpp',['../call__srv_8cpp.html',1,'']]],
-  ['callback_5fsrv_1',['callback_srv',['../call__srv_8cpp.html#a0271c58ef6ee27beb4ca077becc87b76',1,'call_srv.cpp']]]
-];
