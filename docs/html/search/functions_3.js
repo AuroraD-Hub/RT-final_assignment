740256344f1704bf3ee30b2edc5709c88a0f404f
AuroraD-Hub <72380912+AuroraD-Hub@users.noreply.github.com>
2022-03-25 15:47:30 +0000

    Delete docs directory

--- docs/html/search/functions_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['get_5fvar_27',['get_var',['../control_8cpp.html#a9b800862ae6ceb485c2502aad2fcbaeb',1,'get_var():&#160;control.cpp'],['../user__interface_8cpp.html#a9b800862ae6ceb485c2502aad2fcbaeb',1,'get_var():&#160;user_interface.cpp']]]
-];
