9f8b0720e3757aeeb4826968f2b2a6c4b19adb9d
AuroraD-Hub <72380912+AuroraD-Hub@users.noreply.github.com>
2022-04-01 13:31:29 +0000

    Add files via upload

---
+++ docs/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['get_5fvar_11',['get_var',['../control_8cpp.html#a9b800862ae6ceb485c2502aad2fcbaeb',1,'get_var():&#160;control.cpp'],['../user__interface_8cpp.html#a9b800862ae6ceb485c2502aad2fcbaeb',1,'get_var():&#160;user_interface.cpp']]]
+];
